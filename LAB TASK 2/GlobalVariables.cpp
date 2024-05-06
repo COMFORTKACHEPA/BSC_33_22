a4ad055f6ca560f5d32e2d45a33d7e9807cb77d9
COMFORTKACHEPA <bsc-33-22@unima.ac.mw>
2024-05-06 20:54:17 +0200

    assignment 2

---
+++ LAB TASK 2/GlobalVariables.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+// global variable decalration
+int g = 20;
+ 
+ int main (){
+    // local variable
+    g = 10;
+    cout<< g;
+    return 0; 
+ 
+ }
---
+++ LAB TASK 2/LocalVariables.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    //local variables
+    int a, b;
+    int c;
+    //initialization
+    a = 10;
+    b = 20;
+    c = a + c;
+    cout << c;
+
+    return 0;
+}
